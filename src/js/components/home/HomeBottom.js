27c695e4b3ef31925dc83e5b2dc498fd3e9305a6
Vitaliy Shurygin <v.shurygin@theomnicore.com>
2020-11-13 06:19:31 +0200

    add project tabs

--- src/js/components/home/HomeBottom.js
+++ src/js/components/home/HomeBottom.js
@@ -8,7 +8,7 @@ import '../../../css/homepage-slider.scss';
 class HomeBottom extends React.Component {
     render() {
         return (
-            <section className="section section-homepage-bottom">
+            <section className="section section-homepage-bottom projects">
                 <div className="svg-figure svg-figure__orange-left">
                     <img src={ImageFigureLeft} />
                 </div>
@@ -17,8 +17,14 @@ class HomeBottom extends React.Component {
                 </div>
                 <div className="section__content">
                     <h3 className="section-title">Projects for Investment</h3>
-                    <div className="projects__list">
-
+                    <div className="projects-list">
+                        <a href="#" className="projects-list__item">All Industries</a>
+                        <a href="#" className="projects-list__item projects-list__item--active">Tech</a>
+                        <a href="#" className="projects-list__item">Logistics</a>
+                        <a href="#" className="projects-list__item">Aerospace</a>
+                        <a href="#" className="projects-list__item">Agriculture</a>
+                        <a href="#" className="projects-list__item">Medical</a>
+                        <a href="#" className="projects-list__item">More</a>
                     </div>
                 </div>
                 <ProjectsSlider />
